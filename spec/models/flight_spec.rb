855d6534fba0be3140b09526427fa40b8b0d516b
Vanessa Arruda <wanessaarruda@gmail.com>
2021-01-20 09:09:51 -0700

    Add relationships tests

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Flight, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should belong_to :airline }
+    it { should have_many :flight_passengers }
+    it { should have_many(:passengers).through(:flight_passengers) }
+  end
 end
