95fe57ac698518afe8feabeb8bf357313e6e1fa4
Vanessa Arruda <wanessaarruda@gmail.com>
2021-01-20 09:32:46 -0700

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+@airline_1 = Airline.create!(name: "American Airlines")
+@airline_2 = Airline.create!(name: "Frontier")
+
+@flight_1 = @airline_1.flights.create!(number: 123, date: "05/26/2020", departure_city: "Denver", arrival_city: "San Diego")
+@flight_2 = @airline_1.flights.create!(number: 124, date: "05/27/2020", departure_city: "New York", arrival_city: "LA")
+@flight_3 = @airline_2.flights.create!(number: 125, date: "05/28/2020", departure_city: "Boston", arrival_city: "Portland")
+
+@passenger_1 = Passenger.create!(name: "Tom Hanks", age: 52)
+@passenger_2 = Passenger.create!(name: "Bob Marlin", age: 68)
+@passenger_3 = Passenger.create!(name: "Brad Pitte", age: 50)
+
+FlightPassenger.create!(flight_id: @flight_1.id, passenger_id: @passenger_1.id)
+FlightPassenger.create!(flight_id: @flight_1.id, passenger_id: @passenger_2.id)
+FlightPassenger.create!(flight_id: @flight_2.id, passenger_id: @passenger_1.id)
